6b0d189fc46ab15374a1fd86207c76ba943f909b
Md Wajih <mdwajihofficial@gmail.com>
2024-01-08 17:23:14 +0530

    Initializing classes

---
+++ oop.py
@@ -0,0 +1,13 @@
+class employee:
+    def __init__(self, first, last, pay, location):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.location = location
+    
+
+emp1 = employee( "Wajih" ,"ullah" , "1,00,000", "Hyderabad" )
+emp2 = employee("Muneef", "Khan", "50,000", "Mehdipatnam")
+
+print(emp1.first)
+print(emp2.location)
