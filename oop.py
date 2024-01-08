e5ccd632ed6fc766c1187ec9bdf64eb50fa5bbab
Md Wajih <mdwajihofficial@gmail.com>
2024-01-08 17:26:25 +0530

    Adding the fullname function

--- oop.py
+++ oop.py
@@ -1,13 +1,14 @@
 class employee:
-    def __init__(self, first, last, pay, location):
+    def __init__(self, first, last, pay, location, fullname):
         self.first = first
         self.last = last
         self.pay = pay
         self.location = location
+        self.fullname = first + " " + last
     
 
-emp1 = employee( "Wajih" ,"ullah" , "1,00,000", "Hyderabad" )
-emp2 = employee("Muneef", "Khan", "50,000", "Mehdipatnam")
+emp1 = employee( "Wajih" ,"ullah" , "1,00,000", "Hyderabad", "Something" )
+emp2 = employee("Muneef", "Khan", "50,000", "Mehdipatnam", "Something")
 
-print(emp1.first)
-print(emp2.location)
+print(emp1.fullname)
+print(emp2.fullname)
